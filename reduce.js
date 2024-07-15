8de4788a6f2bae8610fb45ab0e981ddbf73162f1
Camilo Villegas <49658932+mrdoomus@users.noreply.github.com>
2024-07-15 22:58:30 +0000

    adding return and removing unused ned of line

--- reduce.js
+++ reduce.js
@@ -1,6 +1,5 @@
 function addAllNumber(arrayOfNumbers) {
-    arrayOfNumbers.reduce((acc, curr) => acc += curr, 0)
-
+    return arrayOfNumbers.reduce((acc, curr) => acc += curr, 0)
 }
 
 addAllNumber([1, 2, 3, 4, 6, 6, 7])
